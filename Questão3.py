51e4028fd5a718e36f6d5d32733874c909d430bb
AdrianFerreira20 <112121482+AdrianFerreira20@users.noreply.github.com>
2024-10-17 11:33:36 -0300

    Add files via upload

---
+++ Questão3.py
@@ -0,0 +1,13 @@
+INDICE = 12
+SOMA = 0
+K = 1
+
+while K < INDICE:
+    K = K + 1
+    SOMA = SOMA + K
+
+print(SOMA)
+#Ao final do processamento, o valor da variável SOMA será 77
+#A cada iteração, K é incrementado em 1 e o valor de K é somado à variável SOMA.
+#O loop para quando K atinge o valor de 12, e o valor final de SOMA será 77.
+#Portanto, ao final do processamento, o valor da variável SOMA será 77
